5cbb18b7062f6c624ab7171809bb9d07d82dfb6e
MirnII <49513103+MirnII@users.noreply.github.com>
2019-04-22 16:35:42 +0900

    v01a
    01a

---
+++ Diplom_1/Diplom_1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Diplom_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string name { get; set; }
+        string roly { get; set; }
+        int status_cod;
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            SQL s = new SQL();
+            status_cod = s.go_in(Log.Text, Pass.Password);
+            if (status_cod == 1)
+            {
+                name = s.Name;
+                roly = s.Roly;
+            }
+            else
+            {
+                Log.BorderBrush = Brushes.Red;
+                Pass.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private void cookie()
+        {
+            if(reme.IsChecked == true)
+            {
+                SQL s = new SQL();
+                s.cookie_(Log.Text, Pass.Password, roly, name);
+            }
+        }
+
+        private void Log_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Log_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Log.Text == "") Log.Text = "Логин";
+
+        }
+
+        private void Pass_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass.Password == "Пароль") Pass.Password = "";
+        }
+
+        private void Pass_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass.Password == "") Pass.Password = "Пароль";
+        }
+
+        private void Log_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Log.Text == "Логин") Log.Text = "";
+        }
+
+        private void Reme_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Diplom_1/Diplom_1/SQL.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Diplom_1
+{
+    class SQL
+    {
+        int status_cod;
+        public string Name { get; set; }
+        public string Roly { get; set; }
+        string Sql = ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;
+        public int go_in(string Login, string Password)
+        {
+            SqlConnection conn = new SqlConnection(Sql);
+            conn.Open();
+            SqlCommand comm = new SqlCommand("Select * from Users", conn);
+            SqlDataReader reader = comm.ExecuteReader();
+            while(reader.Read())
+            {
+                if(Login == reader["Логин"].ToString() && Password == reader["Пароль"].ToString())
+                {
+                    Name = reader["Имя"].ToString();
+                    Roly = reader["Роль"].ToString();
+                    status_cod = 1;
+                    break;
+                }
+                else
+                {
+                    status_cod = 01;
+                }
+            }
+
+            return status_cod;
+        }
+        public void cookie_(string Login, string Password, string Roly_, string Name_)
+        {
+            SqlConnection conn = new SqlConnection(Sql);
+            conn.Open();
+            SqlCommand comm = new SqlCommand("insert into Cookie(ПК, Пользователь_ПК, Логин, Пароль, Имя, Роль) values ('" + Environment.MachineName + "', '" + Environment.UserName + "', '" + Login + "', '" + Password + "', '" + Name_ + "', '" + Roly_ + "')", conn);
+            int num = comm.ExecuteNonQuery();
+        }
+
+    }
+}
